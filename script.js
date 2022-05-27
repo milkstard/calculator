1197471eb46827d0fab65c4c70ebe146e4e819ce
milkstard <jmarkcarbs@gmail.com>
2022-05-27 12:39:47 +0800

    Add an eventListerner to the button-content class and implement an Event Delegation

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const numbers = document.querySelector(".button-content");
+const btns = numbers.addEventListener('click', (event) => {
+    if(event.target.tagName === "BUTTON")
+        console.log(event.target.textContent);
+});
+
+for( let i = 0; i < 10; i++){
+    let numberChild = document.createElement("button");
+    numberChild.classList.toggle("btn-"+i);
+    numberChild.textContent = i;
+    numbers.appendChild(numberChild);
+}
+
 function add(a, b){
     return a+b;
 }
@@ -13,3 +26,20 @@ function multiply(a, b){
 function divide(a, b){
     return a/b;
 }
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case "+":{
+            break;
+        }
+        case "-":{
+            break;
+        }
+        case "*":{
+            break;
+        }
+        case "/":{
+            break;}
+    }
+}
+
