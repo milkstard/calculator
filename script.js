71e451711c6b790cbe564f4410ce6bc2820f83bb
milkstard <jmarkcarbs@gmail.com>
2022-05-29 07:49:37 +0800

    Add resultContent and pressed variables
    it query into html result-content child to manipulate the data
    and added some functionality on clearData() function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let userChoices = [];
+const resultContent = document.querySelector(".result-child-content");
+const pressed = document.querySelector(".pressed");
 const numbers = document.querySelector(".button-content");
 const btns = numbers.addEventListener('click', (event) => {
     
@@ -8,6 +10,12 @@ const btns = numbers.addEventListener('click', (event) => {
             return;
         }
 
+        if(event.target.attributes.class.value === "clear"){
+            clearData();
+            return;
+        }
+
+        pressed.textContent = event.target.textContent;
         let x = event.target.attributes.class.value;
         let y = event.target.textContent;
         const tempObj = {};
@@ -25,20 +33,39 @@ for( let i = 0; i < 10; i++){
     numbers.appendChild(numberChild);
 }
 
-function add(a, b){
+function add(a, b){//creating a default value
+    tempObj['btn'] = a+b;
+    clearData()
+    userChoices.push(tempObj);
     return a+b;
 }
 
 function subtract(a, b){
+    tempObj['btn'] = a-b;
+    clearData()
+    userChoices.push(tempObj);
     return a-b;
 }
 
 function multiply(a, b){
+    tempObj['btn'] = a*b;
+    clearData()
+    userChoices.push(tempObj);
     return a*b;
 }
 
 function divide(a, b){
+    let tempObj ={};
+    tempObj['btn'] = a/b;
+    clearData()
+    userChoices.push(tempObj);
     return a/b;
+}
+
+function clearData(){
+    userChoices = [];
+    resultContent.style.visibility = 'hidden';
+    pressed.textContent = 0;
 }
 
 function process(){
@@ -46,9 +73,10 @@ function process(){
     const q = userChoices.filter(userChoice => userChoice['operator']);
     let y = x.indexOf(q[0]["operator"]);
     const op = q[0]["operator"];
-    const first = x.slice(0,y).join('');
-    const second = x.slice(y+1).join('');
-    operate(op, first, second);
+    const first = parseInt(x.slice(0,y).join(''));
+    const second = parseInt(x.slice(y+1).join(''));
+    resultContent.textContent = operate(op, first, second);
+    resultContent.style.visibility = 'visible';
     //console.log(`${parseInt(first)} ${op} ${parseInt(second)}`);
     //console.log("Array of all contents: " + x);
     //console.log("Index of operator: " + y)
@@ -58,19 +86,19 @@ function process(){
 function operate(operator, num1, num2){
     switch(operator){
         case "+":{
-            console.log(num1+num2);
+            return num1+num2;
             break;
         }
         case "-":{
-            console.log(num1-num2);
+            return num1-num2;
             break;
         }
         case "*":{
-            console.log(num1*num2);
+            return num1*num2;
             break;
         }
         case "/":{
-            console.log(num1/num2);
+            return num1/num2;
             break;}
     }
 }
